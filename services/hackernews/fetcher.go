fd12995ee3e4078db5910eb2b3c5dc1fc38c62fa
alanjames00 <alanjames6282@gmail.com>
2024-12-16 18:10:25 +0530

    feat: concurrent story fetching

--- services/hackernews/fetcher.go
+++ services/hackernews/fetcher.go
@@ -2,6 +2,7 @@
 
 package hackernews
 
+import "fmt"
 
 func FetchTopStories(limit int) ([]Story, error) {
 	ids, err := FetchTopStoryIDs()
@@ -9,20 +10,41 @@ func FetchTopStories(limit int) ([]Story, error) {
 		return nil, err
 	}
 
-	// create an array to store the stories
-	stories := make([]Story, 0, limit)
-	for i, id := range ids {
-		if i >= limit {
-			break
-		}
-
-		story, err := FetchStory(id)
-		if err != nil {
-			continue // skip that id
-		}
-
-		stories = append(stories, story)
+	// limit the number of stories to fetch
+	if (len(ids)) > limit {
+		ids = ids[:limit]
 	}
+
+	// channel to collect fetched stories and errors
+	storiesCh := make(chan Story, limit)
+	errCh := make(chan error, limit)
+
+	// send off goroutines to fetch stories concurrently
+	for _, id := range ids {
+		go func(id int) {
+			story, err := FetchStory(id)
+			if err != nil {
+				errCh <- err
+			}
+			storiesCh <- story
+		}(id)
+	}
+
+	// collect the results
+	stories := make([]Story, 0, limit)
+	for i := 0; i < limit; i++ {
+		select {
+		case story := <- storiesCh:
+			stories = append(stories, story)
+		case err := <- errCh:
+			// log or handle errors, but continue collecting other stories
+			fmt.Printf("error fetching stories: %v", err)
+		}
+	}
+
+	// close both channels
+	close(storiesCh)
+	close(errCh)
 
 	return stories, nil
 }
